c28394c97b0a22e23ed347220d7a65b99ab90646
jmarcantony <josephmarc2610@gmail.com>
2021-12-02 22:51:32 +0400

    Fixed folder structure

---
+++ Day 1/Part 2/main.py
@@ -0,0 +1,9 @@
+with open("input.txt") as f:
+    l = list(map(int, f.readlines()))
+    W = [l[i:i+3] for i in range(len(l))]
+    p = 0
+    for i in range(1, len(l)):
+        if sum(W[i]) > sum(W[i-1]):
+            p += 1
+    print(p)
+

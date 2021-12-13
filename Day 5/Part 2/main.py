b49441bf5c77a768336616ab1b8c5d3b8c58abdf
jmarcantony <josephmarc2610@gmail.com>
2021-12-13 19:02:18 +0400

    Solved day 5 part 2

---
+++ Day 5/Part 2/main.py
@@ -0,0 +1,61 @@
+class Line: 
+    def __init__(self, p1, p2):
+        self.points = []
+        self.p1 = p1
+        self.p2 = p2
+        self.get_points()
+
+    def get_points(self):
+        if self.p1[0] == self.p2[0]:
+            a = 1 if self.p1[1] < self.p2[1] else -1
+            i = self.p1[1]
+            while i != self.p2[1]+a:
+                self.points.append([self.p1[0], i])
+                i += a
+        else:
+            a = 1 if self.p1[0] < self.p2[0] else -1
+            i = self.p1[0]
+            while i != self.p2[0]+a:
+                self.points.append([i, self.p1[1]])
+                i += a
+
+    def get_diagonal(self):
+        x, y, x2, y2 = self.p1[0], self.p1[1], self.p2[0], self.p2[1]
+        a = 1 if x < x2 else -1
+        b = 1 if y < y2 else -1
+        while x != x2 or y != y2:
+            self.points.append([x, y])
+            x += a
+            y += b
+        self.points.append([x2, y2])
+
+    def greatest(self):
+        return max(self.p1 + self.p2)
+
+with open("../input.txt") as f:
+    z = f.readlines()
+    lines = []
+    x = 0
+    for y in z:
+        a, b = y.split(" -> ")
+        p1, p2 = list(map(int, a.split(","))), list(map(int, b.split(",")))
+        l = Line(p1, p2)
+        g = l.greatest()
+        if g > x:
+            x = g+1
+        if (p1[0] == p2[0]) or (p1[1] == p2[1]):
+            lines.append(l)
+        elif abs(p2[0] - p1[0]) == abs(p2[1] - p1[1]):
+            l.points.clear()
+            l.get_diagonal()
+            lines.append(l)
+    graph = [[0 for _ in range(x)] for _ in range(x)]
+    for t in lines:
+        for u in t.points:
+            graph[u[0]-1][u[1]-1] += 1
+    n = 0
+    for g in graph:
+        for h in g:
+            if h > 1:
+                n += 1
+    print(n)
